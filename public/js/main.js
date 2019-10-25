d4f9d0987bb4ca805862bb77d6aaa7b27f22948d
ishibashi.futos <ishibashi.futos@outlook.com>
2019-10-25 13:38:24 +0900

    ヘッダのソート処理を追加

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,3 @@
-window.onerror = (message, source, lineno, colno, error) => {
-  console.log(e)
-}
 const rank = [
   {sidesTotal: 80, rank: "SS"},
   {sidesTotal: 120, rank: "S"},
@@ -13,25 +10,27 @@ const rank = [
   {sidesTotal: 450, rank: "G"},
 ]
 
+const itemsSource = (() => {
+  result = []
+  for (let i = 1; i< 10; i++) {
+    result.push({
+      name: `name_${i.toString().padStart(5, "0")}`,
+      W: Math.round(i * Math.random()) * 10,
+      D: Math.round(i * Math.random()) * 5,
+      H: Math.round(i * Math.random()) * 10,
+      subtotal: ""
+    })
+  }
+  return result
+})()
+
 var app = new Vue({
   el: "#app",
   data: {
     message: "Hello, Vue",
     grid: {
       headers: ["name", "W", "D", "H", "3 sides total", "option", "rank", ""],
-      itemsSource: (() => {
-        result = []
-        for (let i = 1; i< 10; i++) {
-          result.push({
-            name: `name_${i.toString().padStart(5, "0")}`,
-            w: Math.round(i * Math.random()) * 10,
-            d: Math.round(i * Math.random()) * 5,
-            h: Math.round(i * Math.random()) * 10,
-            subtotal: ""
-          })
-        }
-        return result
-      })(),
+      itemsSource: itemsSource
     },
     options: (() => {
       result = []
@@ -48,9 +47,9 @@ var app = new Vue({
       this.grid.itemsSource.splice(i, 1)
     },
     updateSubTotal: function (e) {
-      let index = Number(e.srcElement.id.replace(/[w|h|d]-/, ""))
+      let index = parseInt(e.srcElement.id.replace(/[w|h|d]-/, ""))
       let subtotal = 0
-      "whd".split("").forEach((key) => subtotal += Number(document.getElementById(`${key}-${index}`).value))
+      "whd".split("").forEach((key) => subtotal += parseInt(document.getElementById(`${key}-${index}`).value))
       let updateVal = ""
       this.rank.some((item) => {
         if (subtotal > 460) {
@@ -64,17 +63,26 @@ var app = new Vue({
         return false
       })
       this.grid.itemsSource[index].subtotal = updateVal
+    },
+    sort: function (e) {
+      const thead = document.getElementsByTagName("thead")[0]
+      if (!thead.classList.contains("sort_desc")) {
+        thead.classList.replace("sort_asc", "sort_desc")
+        this.grid.itemsSource = this.grid.itemsSource.sort((a, b) => {
+          const p = e.srcElement.id.replace("_h", "")
+          if(a[p] < b[p]) return -1
+          if(a[p] > b[p]) return 1
+          return 0
+        })
+      } else {
+        thead.classList.replace("sort_desc", "sort_asc")
+        this.grid.itemsSource = this.grid.itemsSource.sort((a, b) => {
+          const p = e.srcElement.id.replace("_h", "")
+          if(a[p] < b[p]) return -1
+          if(a[p] > b[p]) return 1
+          return 0
+        }).reverse()
+      }
     }
   }
-})
-
-let sock = new WebSocket("ws://localhost:3001");
-
-sock.addEventListener("open", (e) => {
-  console.log("connect.")
-})
-
-sock.addEventListener("message", (e) => {
-  console.log(e.data)
-  location.reload()
 })
